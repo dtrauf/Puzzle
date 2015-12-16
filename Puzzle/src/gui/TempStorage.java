fe1f7cdb496521eec96cc09e73f746d91fd8d39f
Daniel Trauf <trauf@hrz.tu-chemnitz.de>
2015-12-16 19:37:47 +0100

    changed attributes

--- Puzzle/src/gui/TempStorage.java
+++ Puzzle/src/gui/TempStorage.java
@@ -4,6 +4,8 @@
 package gui;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * @author <a href="trauf">Daniel Trauf</a>
@@ -13,17 +15,20 @@ import java.awt.Color;
  *
  */
 public class TempStorage extends Territory{
+	
+	private ArrayList<Vector> vertices;
+	
 	private int xPos;
 	private int yPos;
 	private Color c;
-	private int[][]vertices;
+
 	/**
 	 * @param xPos
 	 * @param yPos
 	 * @param c
 	 * @param vertices
 	 */
-	public TempStorage(int xPos, int yPos, Color c, int[][] vertices) {
+	public TempStorage(int xPos, int yPos, Color c, ArrayList<Vector> vertices) {
 		super(pieces);
 		this.xPos = xPos;
 		this.yPos = yPos;
