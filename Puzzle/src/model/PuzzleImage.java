022deed497533a0d292388c22749242a95df0a6a
Daniel Trauf <trauf@hrz.tu-chemnitz.de>
2015-12-17 17:29:27 +0100

    for calculations on image

---
+++ Puzzle/src/model/PuzzleImage.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * Calculation stuff relying on whole puzzle image. Loads an image which is not displayed.
+ * 
+ * @author <a href="trauf@hrz.tu-chemnitz.de">Daniel Trauf</a>
+ *<BR>
+ *		History:<BR>
+ *		<LI>[trauf][17.12.2015] Created</LI>
+ *
+ */
+public class PuzzleImage {
+	
+	private int numberOfPieces;
+	private float pieceWidth;
+	private float pieceHeight;
+	private int resWidth;
+	private int resHeight;
+
+
+}
