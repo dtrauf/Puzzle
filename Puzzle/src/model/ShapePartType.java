cfa2f231adeb973c719f5db3538ccd4053bc65da
Daniel Trauf <trauf@hrz.tu-chemnitz.de>
2016-01-13 18:24:52 +0100

    corrected typographical errors

--- Puzzle/src/model/ShapePartType.java
+++ Puzzle/src/model/ShapePartType.java
@@ -17,10 +17,10 @@ public enum ShapePartType {
 	RIGHT_CONVEXITY("data/shapes/shape03.png"),
 	LOWER_CONVEXITY("data/shapes/shape04.png"),
 	LEFT_CONVEXITY("data/shapes/shape05.png"),
-	UPPER_GAP_FILLLED("data/shapes/shape06.png"),
-	RIGHT_GAP_FILLLED("data/shapes/shape07.png"),
-	LOWER_GAP_FILLLED("data/shapes/shape08.png"),
-	LEFT_GAP_FILLLED("data/shapes/shape09.png");	
+	UPPER_GAP_FILLED("data/shapes/shape06.png"),
+	RIGHT_GAP_FILLED("data/shapes/shape07.png"),
+	LOWER_GAP_FILLED("data/shapes/shape08.png"),
+	LEFT_GAP_FILLED("data/shapes/shape09.png");	
 	
 	
 	private String typeImagePath;
